1cbcb4426a6025e2906d0ac9373f45c6d812d488
刘凯 <354378485@qq.com>
2020-09-02 00:11:20 +0800

    '抽取bean和service至api项目'

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-哔哩哔哩视频看到：https://www.bilibili.com/video/BV1LK4y1b7Ds?p=17
+哔哩哔哩视频看到：https://www.bilibili.com/video/BV1LK4y1b7Ds?p=18（未看完）
 
 ![域名的配置](https://www.bilibili.com/video/BV1LK4y1b7Ds?p=9)
 
@@ -15,18 +15,3 @@ gmall-parent : 统一管理pom依赖
 
 ## Idea @AutoWiried 误报
 Settings --> 搜索“inspect” --> spring --> code --> [AutoWiring for Bean Class]不打勾
-
-```xml
-<?xml version="1.0" encoding="UTF-8"?>
-<project xmlns="http://maven.apache.org/POM/4.0.0"
-         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
-    <modelVersion>4.0.0</modelVersion>
-
-    <groupId>com.atguigu</groupId>
-    <artifactId>gmall</artifactId>
-    <version>1.0-SNAPSHOT</version>
-
-
-</project>
-```
---
+++ gmall-api/src/main/java/com/atguigu/gmall/bean/UmsMember.java
@@ -0,0 +1,177 @@
+package com.atguigu.gmall.bean;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+
+public class UmsMember implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String levelId; //会员等级id
+    private String username; //用户名
+    private String password; //密码
+    private String nickname; //昵称
+    private String mobile;   //手机号码
+    private String email;    //邮箱
+    private String header;  //头像
+    private int gender;    //性别
+    private Date birth;    //生日
+    private String city;   //所在城市
+    private String job;    //职业
+    private String sign;   //个性签名
+    private int sourceType;  //用户来源
+    private int integration;  //积分
+    private int growth;     //成长值
+    private int status;     //启用状态
+    private Date createTime;  //注册时间
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLevelId() {
+        return levelId;
+    }
+
+    public void setLevelId(String levelId) {
+        this.levelId = levelId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+    public int getSourceType() {
+        return sourceType;
+    }
+
+    public void setSourceType(int sourceType) {
+        this.sourceType = sourceType;
+    }
+
+    public int getIntegration() {
+        return integration;
+    }
+
+    public void setIntegration(int integration) {
+        this.integration = integration;
+    }
+
+    public int getGrowth() {
+        return growth;
+    }
+
+    public void setGrowth(int growth) {
+        this.growth = growth;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+
+}
